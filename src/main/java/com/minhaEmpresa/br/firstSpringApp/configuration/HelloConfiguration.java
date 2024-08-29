234787957b14f1245294d5569fbb6c290109977a
Victor Emanuel <victor.emanuel@sgnsistemas.com.br>
2024-08-29 15:30:21 -0300

    entendendo injeção de dependencia no java spring e @configuration e @Bean

---
+++ src/main/java/com/minhaEmpresa/br/firstSpringApp/configuration/HelloConfiguration.java
@@ -0,0 +1,13 @@
+package com.minhaEmpresa.br.firstSpringApp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+    //    Cria instancia de classe que nao pode ser gerenciada pelo spring como  classes de origem de biblioteca de terceiros
+    @Bean
+    public SDKAWS SDKAws() {
+        return new SDKAWS();
+    }
+}
--- src/main/java/com/minhaEmpresa/br/firstSpringApp/controllers/HelloWorldController.java
+++ src/main/java/com/minhaEmpresa/br/firstSpringApp/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.minhaEmpresa.br.firstSpringApp.controllers;
 
 import com.minhaEmpresa.br.firstSpringApp.services.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloWorldController {
+    @Autowired
     private HelloWorldService helloWorldService;
-
-    public HelloWorldController(HelloWorldService helloWorldService) {
-        this.helloWorldService = helloWorldService;
-    }
+// ao inves de colocar o construtor fazendo a injeção podemos fazer usando @Autowide
+//    public HelloWorldController(HelloWorldService helloWorldService) {
+//        this.helloWorldService = helloWorldService;
+//    }
 
     @GetMapping
     public String helloWorld() {
